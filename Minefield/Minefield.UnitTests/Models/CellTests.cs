4de5cbc50a544a4b92f885e63d018f9a3bf5e2ab
MaceYaFace <nfs2012hp@live.co.uk>
2025-01-28 16:59:48 +0000

    Templated tests for Cell player flag functionality

---
+++ Minefield/Minefield.UnitTests/Models/CellTests.cs
@@ -0,0 +1,14 @@
+﻿using Minefield.Models;
+
+namespace Minefield.UnitTests.Models;
+
+public class CellTests
+{
+    [Test]
+    public void Given_Cell_When_PlayerOnCellCalled_Then_CellIsCheckedAndIsPlayer()
+    {
+        // Create cell object
+        // Call PlayerOnCell method
+        // Assert that IsChecked and IsPlayer are true
+    }
+}
