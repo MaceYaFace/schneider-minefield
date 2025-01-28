7c09808d500a2bd0e236336725607d340a0dfa52
MaceYaFace <nfs2012hp@live.co.uk>
2025-01-28 16:15:59 +0000

    Added Minefield test template

---
+++ Minefield/Minefield.UnitTests/Models/MinefieldTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Minefield.UnitTests.Models;
+
+public class MinefieldTests
+{
+    public void Given_Minefield_When_GenerateCellsCalled_Then_CellsGeneratedWithMines()
+    {
+        // Create Minefield object
+        
+        // Call GenerateCells
+        
+        // Assert that cells generated and contains the correct proportion of mines
+    }
+}
