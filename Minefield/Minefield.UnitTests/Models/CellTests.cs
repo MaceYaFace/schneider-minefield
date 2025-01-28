8f98c9e440341e447908c7e6c6b38ea4531de42e
MaceYaFace <nfs2012hp@live.co.uk>
2025-01-28 17:00:21 +0000

    Added IsPlayer flag functionality to cell model

--- Minefield/Minefield.UnitTests/Models/CellTests.cs
+++ Minefield/Minefield.UnitTests/Models/CellTests.cs
@@ -7,8 +7,13 @@ public class CellTests
     [Test]
     public void Given_Cell_When_PlayerOnCellCalled_Then_CellIsCheckedAndIsPlayer()
     {
-        // Create cell object
-        // Call PlayerOnCell method
-        // Assert that IsChecked and IsPlayer are true
+        var cell = new Cell();
+        
+        cell.PlayerOnCell();
+        Assert.Multiple(() =>
+        {
+            Assert.That(cell.IsChecked, Is.True);
+            Assert.That(cell.IsPlayer, Is.True);
+        });
     }
 }
--- Minefield/Minefield/Models/Cell.cs
+++ Minefield/Minefield/Models/Cell.cs
@@ -5,4 +5,11 @@ public class Cell
     public Coordinates Coordinates { get; set; }
     public bool IsMine { get; set; }
     public bool IsChecked { get; set; } = false;
+    public bool IsPlayer { get; set; } = false;
+
+    public void PlayerOnCell()
+    {
+        IsChecked = true;
+        IsPlayer = true;
+    }
 }
