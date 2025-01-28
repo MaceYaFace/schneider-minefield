24e4bc2838b1ab560f67ae0327af88bdf040ad38
MaceYaFace <nfs2012hp@live.co.uk>
2025-01-28 15:12:53 +0000

    Created initial projects and templated player character lives tests

---
+++ Minefield/Minefield.UnitTests/Models/PlayerCharacterTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Minefield.UnitTests.Models;
+
+[TestFixture]
+public class PlayerCharacterTests
+{
+    [Test]
+    public void Given_PlayerCharacterWithLives_When_RemoveLifeCalled_Then_LivesDecreasedByOne()
+    {
+        // Create PlayerCharacter object
+
+        // Call RemoveLife
+
+        // Assert life removed
+    }
+
+    [Test]
+    public void Given_PlayerCharacterRemoveLifeCalled_When_LivesAreZero_Then_CorrectExceptionThrown()
+    {
+        // Create PlayerCharacter object
+
+        // Call RemoveLife
+
+        // Assert exception thrown
+    }
+}
