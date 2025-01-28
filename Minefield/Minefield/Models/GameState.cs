3c8fa080c3fafdc0bc616f1405e2bd6c9d20c6a6
MaceYaFace <nfs2012hp@live.co.uk>
2025-01-28 19:23:04 +0000

    Added game state model

---
+++ Minefield/Minefield/Models/GameState.cs
@@ -0,0 +1,9 @@
+﻿namespace Minefield.Models;
+
+public class GameState
+{
+    public Cell[] Cells { get; set; }
+    public uint Lives { get; set; }
+    public uint Moves { get; set; }
+    public bool PlayerOnMine { get; set; }
+}
